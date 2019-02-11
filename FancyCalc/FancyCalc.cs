113a6afb20f8095dbc95aebe5307f5d0ffa1b529
Nikita <40839818+nikita161095@users.noreply.github.com>
2019-02-12 02:08:37 +0300

    culculate

--- FancyCalc/FancyCalc.cs
+++ FancyCalc/FancyCalc.cs
@@ -31,8 +31,35 @@ namespace FancyCalc
         //generic calc method. usage: "10 + 20"  => result 30
         public double Culculate(string expression)
         {
-            throw new NotImplementedException();
-
+            //throw new NotImplementedException();
+            int pos_sign = expression.IndexOfAny(new char[] { '+', '-', '*', '/' });
+            int pos_equal = expression.IndexOf('=');
+            if (pos_equal == -1)
+            {
+                pos_equal = expression.Length;
+            }
+            double a = double.Parse(expression.Substring(0, pos_sign));
+            double b = double.Parse(expression.Substring(pos_sign + 1, pos_equal - 1 - pos_sign));
+            if (expression.IndexOf('+') != -1)
+            {
+                return a + b;
+            }
+            else if (expression.IndexOf('-') != -1)
+            {
+                return a - b;
+            }
+            else if (expression.IndexOf('*') != -1)
+            {
+                return a * b;
+            }
+            else if (expression.IndexOf('/') != -1)
+            {
+                return a / b;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
